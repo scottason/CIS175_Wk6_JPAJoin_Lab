bf6c96ea1a08da59205a4d83a8808777f6160720
Anthony Hamlin <adhamlingithub@gmail.com>
2020-02-24 09:58:42 -0600

    Copy of consoleshoppinglist for wk6 lab

---
+++ src/main/java/model/ListItem.java
@@ -0,0 +1,65 @@
+package model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="items")
+public class ListItem {
+	//Anthony Hamlin 
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@Column(name="ID")
+	private int id;
+	@Column(name="STORE")
+	private String store;
+	@Column(name="ITEM")
+	private String item;
+	
+	public ListItem() {
+		super();
+	}
+	
+
+	public ListItem(String store, String item) {
+		super();
+		this.store = store;
+		this.item = item;
+	}
+
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getStore() {
+		return store;
+	}
+
+	public void setStore(String store) {
+		this.store = store;
+	}
+
+	public String getItem() {
+		return item;
+	}
+
+	public void setItem(String item) {
+		this.item = item;
+	}
+	
+	
+	public String returnItemDetails() {
+		return store + ": " + item; 
+	}
+
+}
